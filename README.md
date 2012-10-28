07f75a3cc3b872aed8c106011ed245f06d97357b
Kai Hendry <hendry@webconverger.com>
2012-10-28 10:43:09 +0000

    publishing to github

---
+++ README.md
@@ -0,0 +1,8 @@
+# Location log, loc-log, ll
+
+Demo: http://ll.dabase.com
+
+## BUGS
+
+* p.timestamp on ios6 is very wrong, using Date.now(), instead
+* how do you copy the [object Coordinates] ? JSON.stringify doesn't work
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var p = {};
+var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false );
+
 function successCallback(p) {
 
 	var d = Date.now();
@@ -6,8 +9,7 @@ function successCallback(p) {
 
 	s = document.getElementById("status");
 	s.innerHTML = '<dt>timestamp</dt><dd>' + new Date(p.timestamp) + '</dd>';
-	s.innerHTML += '<dt>latitude</dt><dd>' + p.coords.latitude + '</dd>';
-	s.innerHTML += '<dt>longitude</dt><dd>' + p.coords.longitude + '</dd>';
+	s.innerHTML += '<dt>latitude</dt><dd>' + p.coords.latitude + ',' + p.coords.longitude + '</dd>';
 	s.innerHTML += '<dt>accuracy</dt><dd>' + p.coords.accuracy + '</dd>';
 	if(p.coords.altitude) { s.innerHTML += '<dt>altitude</dt><dd>' + p.coords.altitude + '</dd>'; }
 	if(p.coords.altitudeAccuracy) { s.innerHTML += '<dt>altitude Accuracy</dt><dd>' + p.coords.altitudeAccuracy + '</dd>'; }
@@ -15,19 +17,30 @@ function successCallback(p) {
 	if(p.coords.speed) { s.innerHTML += '<dt>speed</dt><dd>' + p.coords.speed + '</dd>'; }
 
 	c = document.getElementById("crumbs");
-	for (var i=0; i < localStorage.length; i++) {
-		var k = localStorage.key(i);
-		console.log("key" + k);
-		var t = new Date(parseInt(k)).toRelativeTime();
-		console.log(t);
-		var v = localStorage.getItem(k);
-		console.log("foo" + v);
-		c.innerHTML += '<li>' + t + ': ' + v + '&mdash;<a href="maps:?q=' + v + '">on a map</a></li>';
-	}
 
+	var localStorageKeys = Object.keys(localStorage);
+	localStorageKeys.sort();
+	localStorageKeys.reverse();
+
+	for (var k in localStorageKeys){
+		var t = new Date(parseInt(localStorageKeys[k],10)).toRelativeTime();
+		console.log(t);
+		var v = localStorage.getItem(localStorageKeys[k]);
+		var s = '<li>' + t + ': ' + v + '&mdash;<a href="';
+		if (iOS) {
+		s += 'maps:?q=' + v + '">on a map</a></li>';
+		} else {
+		s += 'https://maps.google.com/maps?ll=' + v + '">on a map</a></li>';
+		}
+		c.innerHTML += s;
+	}
 }
 
-var p = {};
+function eat() {
+	c = document.getElementById("crumbs");
+	c.innerHTML = "";
+	localStorage.clear();
+}
 
 function errorCallback(e) {
 	s = document.getElementById("status");
@@ -36,7 +49,5 @@ function errorCallback(e) {
 }
 
 function geohello() {
-	navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
-		maximumAge: 600000
-	});
+	navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 }
